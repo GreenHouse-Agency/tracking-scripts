e4f509b3885f333acd3db22812509f8b106b58da
Justin Buys <113367853+Justin-Buys@users.noreply.github.com>
2024-07-24 10:35:05 -0400

    Update ml_field_tracker.js

--- MeridianLink/ml_field_tracker.js
+++ MeridianLink/ml_field_tracker.js
@@ -23,174 +23,8 @@
 'use strict';
 
 class Application {
-  constructor(type) {
-    const collectFields = {
-        "CC" : [
-            "txtFName",
-            "txtLName",
-            "txtEmail",
-            "txtMobilePhone",
-            "txtHomePhone",
-            "txtWorkPhone",
-            "preferredContactMethod",
-            "ddlOccupyingStatus",
-            "ddlOccupancyDurationYear",
-            "ddlOccupancyDurationMonth",
-            "txtGrossMonthlyIncome",
-            "ddlEmploymentStatus",
-            "txtTotalMonthlyHousingExpense",
-            "hdloanDtl",
-            "hdCcSelectedLoanPurpose",
-            "txtRequestCreditLimit",
-            "ddlCreditCardType",
-            "hdCcSelectedCardName"
-        ],
-        "PL" : [
-            "txtFName",
-            "txtLName",
-            "txtEmail",
-            "txtMobilePhone",
-            "txtHomePhone",
-            "txtWorkPhone",
-            "preferredContactMethod",
-            "ddlOccupyingStatus",
-            "ddlOccupancyDurationYear",
-            "ddlOccupancyDurationMonth",
-            "txtGrossMonthlyIncome",
-            "ddlEmploymentStatus",
-            "txtTotalMonthlyHousingExpense",
-            "hdloanDtl",
-            "hdLoanPurpose",
-            "hdIsLineOfCredit",
-            "txtLoanAmount"
-        ],
-        "VL" : [
-            "txtFName",
-            "txtLName",
-            "txtEmail",
-            "txtMobilePhone",
-            "txtHomePhone",
-            "txtWorkPhone",
-            "preferredContactMethod",
-            "ddlOccupyingStatus",
-            "ddlOccupancyDurationYear",
-            "ddlOccupancyDurationMonth",
-            "txtGrossMonthlyIncome",
-            "ddlEmploymentStatus",
-            "txtTotalMonthlyHousingExpense",
-            "hdloanDtl",
-            "hdLoanPurpose",
-            "ddlVehicleType",
-            "txtProposedLoanAmount"
-        ],
-        "HE" : [
-            "txtFName",
-            "txtLName",
-            "txtEmail",
-            "txtMobilePhone",
-            "txtHomePhone",
-            "txtWorkPhone",
-            "preferredContactMethod",
-            "ddlOccupyingStatus",
-            "ddlOccupancyDurationYear",
-            "ddlOccupancyDurationMonth",
-            "txtGrossMonthlyIncome",
-            "ddlEmploymentStatus",
-            "txtTotalMonthlyHousingExpense",
-            "hdloanDtl",
-            "hdLoanPurpose",
-            "ddlBranchName",
-            "txtLoanRequestAmount",
-            "ddlLoanReason"
-        ],
-        "card" : [
-            "bl_BusinessInfotxtCompanyName",
-            "bl_BusinessInfoddlEntityType",
-            "bl_BusinessInfoddlIndustry",
-            "bl_BusinessInfoddlBusinessStatus",
-            "bl_BusinessInfotxtEmail",
-            "bl_BusinessInfotxtMobilePhone",
-            "bl_BusinessInfotxtHomePhone",
-            "bl_BusinessInfotxtFax",
-            "bl_BusinessInfoWebsiteURL",
-            "bl_BusinessInfotxtGrossMonthlyIncome",
-            "bl_BusinessInfoddlOccupyingStatus",
-            "bl_BusinessInfoddlOccupancyDurationYear",
-            "bl_BusinessInfoddlOccupancyDurationMonth",
-            "ba_Primary_0txtFName",
-            "ba_Primary_0txtLName",
-            "ba_Primary_0preferredContactMethod",
-            "ba_Primary_0txtContactEmail",
-            "ba_Primary_0txtContactHomePhone",
-            "ba_Primary_0txtContactWorkPhone",
-            "ba_Primary_0txtContactWorkPhoneExt",
-            "ba_Primary_0txtContactMobilePhone",
-            "hdloanDtl",
-            "ddlBranchName",
-            "hdLoanPurpose",
-            "ddlCreditCardType",
-            "txtLoanReason"
-        ],
-        "vehicle" : [
-            "bl_BusinessInfotxtCompanyName",
-            "bl_BusinessInfoddlEntityType",
-            "bl_BusinessInfoddlIndustry",
-            "bl_BusinessInfoddlBusinessStatus",
-            "bl_BusinessInfotxtEmail",
-            "bl_BusinessInfotxtMobilePhone",
-            "bl_BusinessInfotxtHomePhone",
-            "bl_BusinessInfotxtFax",
-            "bl_BusinessInfoWebsiteURL",
-            "bl_BusinessInfotxtGrossMonthlyIncome",
-            "bl_BusinessInfoddlOccupyingStatus",
-            "bl_BusinessInfoddlOccupancyDurationYear",
-            "bl_BusinessInfoddlOccupancyDurationMonth",
-            "ba_Primary_0txtFName",
-            "ba_Primary_0txtLName",
-            "ba_Primary_0preferredContactMethod",
-            "ba_Primary_0txtContactEmail",
-            "ba_Primary_0txtContactHomePhone",
-            "ba_Primary_0txtContactWorkPhone",
-            "ba_Primary_0txtContactWorkPhoneExt",
-            "ba_Primary_0txtContactMobilePhone",
-            "hdloanDtl",
-            "ddlBranchName",
-            "hdLoanPurpose",
-            "ddlVehicleType",
-            "txtProposedLoanAmount",
-            "ddlLoanTerm",
-            "txtLoanReason"
-        ],
-        "other" : [
-            "bl_BusinessInfotxtCompanyName",
-            "bl_BusinessInfoddlEntityType",
-            "bl_BusinessInfoddlIndustry",
-            "bl_BusinessInfoddlBusinessStatus",
-            "bl_BusinessInfotxtEmail",
-            "bl_BusinessInfotxtMobilePhone",
-            "bl_BusinessInfotxtHomePhone",
-            "bl_BusinessInfotxtFax",
-            "bl_BusinessInfoWebsiteURL",
-            "bl_BusinessInfotxtGrossMonthlyIncome",
-            "bl_BusinessInfoddlOccupyingStatus",
-            "bl_BusinessInfoddlOccupancyDurationYear",
-            "bl_BusinessInfoddlOccupancyDurationMonth",
-            "ba_Primary_0txtFName",
-            "ba_Primary_0txtLName",
-            "ba_Primary_0preferredContactMethod",
-            "ba_Primary_0txtContactEmail",
-            "ba_Primary_0txtContactHomePhone",
-            "ba_Primary_0txtContactWorkPhone",
-            "ba_Primary_0txtContactWorkPhoneExt",
-            "ba_Primary_0txtContactMobilePhone",
-            "hdloanDtl",
-            "ddlBranchName",
-            "hdLoanPurpose",
-            "txtLoanAmount",
-            "txtLoanTerm",
-            "txtLoanReason"
-        ],
-    };
+  constructor(type, gtmContainer) {
+    const collectFields = google_tag_manager[gtmContainer].dataLayer.get('collectFields');
 
     this.trackFields = collectFields[type];
 
@@ -224,11 +58,11 @@ class Application {
   }
 }
 
-function initApplication() {
-    const loanType = document.getElementById("hdnLoanType");
-    const busLoanType = document.getElementById("hdBusinessLoanType");
-    const appType = loanType ? loanType.value : busLoanType.value;
+function initApplication(gtmContainer) {
+  const loanType = document.getElementById("hdnLoanType");
+  const busLoanType = document.getElementById("hdBusinessLoanType");
+  const appType = loanType ? loanType.value : busLoanType.value;
 
-    // init accordions
-    new Application(appType);
+  // init accordions
+  new Application(appType, gtmContainer);
 };
